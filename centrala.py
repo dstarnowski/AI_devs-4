50a3c3b92a7a56e8b84b9867010143f199253f97
Daniel Starnowski <r-gith@dasiu.pl>
2026-03-23 01:10:56 +0100

    Centrala new features
    - allow not to load a file (just run empty HTTP "GET", e.g. to reset task)

--- centrala.py
+++ centrala.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 
@@ -6,12 +7,18 @@ class Centrala:
         self.server_url = server_url
         self.api_key = api_key
 
-    def get_file(self, server_filename: str, local_filepath: str):
+    def get_file(self, server_filename: str, local_filepath: str | None):
         url = f"{self.server_url}data/{self.api_key}/{server_filename}"
         response = requests.get(url)
         response.raise_for_status()
-        with open(local_filepath, "wb") as f:
-            f.write(response.content)
+        if local_filepath is not None:
+            with open(local_filepath, "wb") as f:
+                f.write(response.content)
+        return response.content
+
+    def get_flags(self, response_json) -> str:
+        matches = re.findall(r'\{FLG:([^}]+)\}', str(response_json))
+        return "\n".join(matches)
 
     def send_result(self, task: str, answer) -> requests.Response:
         payload = {
